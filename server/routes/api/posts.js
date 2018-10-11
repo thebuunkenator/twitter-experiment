347384f8d8faa72a0eecf25e856e9017324f4dc6
Erik Buunk <erik.buunk@gmail.com>
2018-10-12 00:30:33 +0200

    post and delete posts

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -9,8 +9,22 @@ router.get('/', async (req, res) => {
   res.send(await posts.find().toArray())
 })
 //add post
+router.post('/', async (req, res) => {
+  const posts = await loadPostsCollection()
+  await posts.insertOne({
+    text: req.body.text,
+    createdAt: new Date()
+  })
+  res.status(201).send()
+})
+//delete post
 
-//delete post 
+router.delete('/:id', async (req, res) => {
+  const posts = await loadPostsCollection() 
+  await posts.deleteOne({_id: new mongodb.ObjectID(req.params.id)})
+  res.status(200).send()
+})
+
 async function loadPostsCollection() {
   const client = await
   mongodb.MongoClient.connect('mongodb://vue_express:vue123@ds239029.mlab.com:39029/vue_express', {
