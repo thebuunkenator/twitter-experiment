9f7da20aefe5278ceb1eb8eb9bfa3f3d578148a6
Erik Buunk <erik.buunk@gmail.com>
2018-10-12 21:45:57 +0200

    double click om te verwijderen toegevoegd

--- client/src/components/PostComponent.vue
+++ client/src/components/PostComponent.vue
@@ -18,6 +18,7 @@
         v-bind:item="post"
         v-bind:index="index"
         v-bind:key="post._id"
+        v-on:dblclick="deletePost(post._id)"
       >
         {{ `${post.createdAt.getDate()}/${post.createdAt.getMonth()}/
         ${post.createdAt.getFullYear()}` }}
@@ -50,6 +51,10 @@ export default {
   methods: {
     async createPost() {
       await PostService.insertPost(this.text);
+      this.posts = await PostService.getPosts();
+    },
+    async deletePost(id) {
+      await PostService.deletePost(id);
       this.posts = await PostService.getPosts();
     } 
   }
