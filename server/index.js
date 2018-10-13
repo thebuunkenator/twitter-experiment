5142320ca430e7ed7903b691af08fc174cd115a5
Erik Buunk <erik.buunk@gmail.com>
2018-10-13 08:26:30 +0200

    development instellingen voor url en redirection

--- server/index.js
+++ server/index.js
@@ -13,6 +13,14 @@ const posts = require('./routes/api/posts')
 //every call for api posts gets redirected to posts
 app.use('/api/posts', posts)
 
+//Handle production
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(__dirname+ '/public')) //use /public as static folder
+  // handel SPA
+   //all andere routes worden naar index.html geleid  (muv bovengenoemde van de api)
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'))
+}
+
 //start server
 const port = process.env.PORT || 5000
 app.listen(port, () => console.log(`Server running on port ${port}`))
